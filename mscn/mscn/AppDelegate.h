832e0cb8b12cc29114f4e555e3842ed3820049dc
hoangnn-lev <hoangnn@leverages.jp>
2013-12-24 02:09:03 -0800

    Design login

---
+++ mscn/mscn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mscn
+//
+//  Created by Nguyen Hoang on 12/24/13.
+//  Copyright (c) 2013 Hoang Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
